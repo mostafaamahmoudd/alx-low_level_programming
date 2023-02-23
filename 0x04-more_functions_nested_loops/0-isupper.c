47cc7fa13adcfcdbeb1dd44611ca0ba07284ff3c
Mostafa <105555194+mostafaamahmoudd@users.noreply.github.com>
2023-02-23 17:47:46 +0200

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -10,13 +10,3 @@ int _isupper(int c){
         return 0;
     }
 }
-int main(void)
-{
-    char c;
-
-    c = 'A';
-    printf("%c: %d\n", c, _isupper(c));
-    c = 'a';
-    printf("%c: %d\n", c, _isupper(c));
-    return (0);
-}
