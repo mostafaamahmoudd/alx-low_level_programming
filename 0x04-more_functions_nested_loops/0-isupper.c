8c4326d979663958b53227a205ee9f339957b635
mostafaamahmoudd <mostafamahmoud805554@std.mans.edu.eg>
2023-02-23 17:44:48 +0200

    exe

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int _isupper(int c){
+    if ((c <= 90) && (c >= 65)){
+        return 1;
+    }else {
+        return 0;
+    }
+}
+int main(void)
+{
+    char c;
+
+    c = 'A';
+    printf("%c: %d\n", c, _isupper(c));
+    c = 'a';
+    printf("%c: %d\n", c, _isupper(c));
+    return (0);
+}
