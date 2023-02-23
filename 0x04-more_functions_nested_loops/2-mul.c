606db70362e72ed8d1ba8dceabaa52817dc9896e
mostafaamahmoudd <mostafamahmoud805554@std.mans.edu.eg>
2023-02-23 18:10:43 +0200

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+* _isdigit - write a function that check for a digit (0 through 9).
+* @c: char to check
+* Return:  0 or 1
+**/
+int mul(int a, int b){
+    return a * b;
+}
